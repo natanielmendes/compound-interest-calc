2733259c4b00b5cfd0f558255ab593efa2efe1c7
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-07-01 23:43:55 -0300

    Updates

---
+++ precache-manifest.23d8593de3ba35757c3443a211162d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5c6e42df0dd217d0b4504121ac66f1",
+    "url": "/compound-interest-calculator/index.html"
+  },
+  {
+    "revision": "2147e9007b8787552b94",
+    "url": "/compound-interest-calculator/static/css/main.5709d7be.chunk.css"
+  },
+  {
+    "revision": "108094ef12b5b3657d41",
+    "url": "/compound-interest-calculator/static/js/2.d870990c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/compound-interest-calculator/static/js/2.d870990c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2147e9007b8787552b94",
+    "url": "/compound-interest-calculator/static/js/main.b11d3d4f.chunk.js"
+  },
+  {
+    "revision": "76a7cfa9f04808a77a18",
+    "url": "/compound-interest-calculator/static/js/runtime-main.eaef40fa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/compound-interest-calculator/precache-manifest.65bf3d3a9409bdfd9882cbcc628df1db.js"
+  "/compound-interest-calculator/precache-manifest.23d8593de3ba35757c3443a211162d0a.js"
 );
 
 self.addEventListener('message', (event) => {
